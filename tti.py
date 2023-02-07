c5a06a8a7741ed114afb2fae0e4bf2edc44b67d9
Barna1 <95107285+Barna1@users.noreply.github.com>
2023-02-07 12:21:02 +0100

    zz6585785

--- tti.py
+++ tti.py
@@ -1,33 +1,31 @@
-file=open("adatok.txt", "r")
+file=open("adatok.txt","r")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 next(file)
+#1.
+print(len(file.readlines()),"ember adatait tartalmazza")
+#3.
+otvenalatt=0
+for i in range(len(file)):
+    if [i][2]<50:
+        otvenalatt+=1
+print(otvenalatt)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#2. feladat
-
-magassagok=[]
-for sor in file:
-    adatok=sor.strip().split("\t")
-    magassag=int(adatok[1])
-    magassagok.append(magassag)
-
-file.close()
-
-atlagmag=sum(magassagok)/len(magassagok)
-atlagmag=round(atlagmag,2)
-print("Az átlagmagasság: ", atlagmag, "cm")
+#6.
+ttiindex=open("index.txt","w")
+for i in file:
+    if([i][1]==18 or 25):
+        print("normális")
