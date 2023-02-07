5fe9f090f1d5a313e5aaa05a2a7a8ca7aea4ecf4
Fleri <floriankemeny@gmail.com>
2023-02-07 12:00:48 +0100

    2. feladat

--- tti.py
+++ tti.py
@@ -1,3 +1,17 @@
-file=open("adatok.txt","r")
-Ladatok=[]
-print(file.read())
+file=open("adatok.txt", "r")
+
+next(file)
+
+#2. feladat
+
+magassagok=[]
+for sor in file:
+    adatok=sor.strip().split("\t")
+    magassag=int(adatok[1])
+    magassagok.append(magassag)
+
+file.close()
+
+atlagmag=sum(magassagok)/len(magassagok)
+atlagmag=round(atlagmag,2)
+print("Az átlagmagasság: ", atlagmag, "cm")
