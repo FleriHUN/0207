d4089ef37f515692b23ffe2360c792160b611974
Fleri <floriankemeny@gmail.com>
2023-02-07 12:10:22 +0100

    sdfgdfgsdfg

--- tti.py
+++ tti.py
@@ -1,10 +1,33 @@
-file=open("adatok.txt","r")
+file=open("adatok.txt", "r")
 
 next(file)
-#1.
-print(len(file.readlines()),"ember adatait tartalmazza")
-#6.
-ttiindex=open("index.txt","w")
-for i in file:
-    if([i][1]==18 or 25):
-        print("normális")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#2. feladat
+
+magassagok=[]
+for sor in file:
+    adatok=sor.strip().split("\t")
+    magassag=int(adatok[1])
+    magassagok.append(magassag)
+
+file.close()
+
+atlagmag=sum(magassagok)/len(magassagok)
+atlagmag=round(atlagmag,2)
+print("Az átlagmagasság: ", atlagmag, "cm")
