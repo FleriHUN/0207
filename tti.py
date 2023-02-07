9b2ba784233f5a87d5553cf14c4c74464eb8aa55
Barna1 <95107285+Barna1@users.noreply.github.com>
2023-02-07 12:04:44 +0100

    markus

--- tti.py
+++ tti.py
@@ -1,3 +1,11 @@
 file=open("adatok.txt","r")
-Ladatok=[]
-print(file.read())
+
+next(file)
+#1.
+for i in file:
+    print(Ladatok.append([i](len)))
+#6.
+ttiindex=open("index.txt","w")
+for i in file:
+    if([i][1]==18 or 25):
+        print("normális")
