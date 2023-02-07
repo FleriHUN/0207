27a8e778c129d4acbe3fc587f3b4e6d26ec863ab
Fleri <floriankemeny@gmail.com>
2023-02-07 12:21:05 +0100

    vvx

--- tti.py
+++ tti.py
@@ -2,7 +2,15 @@ file=open("adatok.txt", "r")
 
 next(file)
 
+magassagok=[]
+for sor in file:
+    adatok=sor.strip().split("\t")
+    magassag=int(adatok[1])
+    magassagok.append(magassag)
 
+file.close()
+
+#1. feladat
 
 
 
@@ -20,14 +28,31 @@ next(file)
 
 #2. feladat
 
-magassagok=[]
-for sor in file:
-    adatok=sor.strip().split("\t")
-    magassag=int(adatok[1])
-    magassagok.append(magassag)
 
-file.close()
 
 atlagmag=sum(magassagok)/len(magassagok)
 atlagmag=round(atlagmag,2)
 print("Az átlagmagasság: ", atlagmag, "cm")
+
+#3. feladat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#4.
+
+
